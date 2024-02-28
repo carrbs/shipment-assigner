7dc7907fd6666377408d5f76a7afbce1bf6e5efc
Ben Carr <carrbens@gmail.com>
2024-02-28 15:52:29 -0800

    export some functions for testing

--- suitabilityScore.ts
+++ suitabilityScore.ts
@@ -28,14 +28,14 @@ function getCommonFactor(a: number, b: number): number {
   return b === 0 ? a : getCommonFactor(b, a % b);
 }
 
-function calculateSS(d: string, s: string): number {
+export function calculateSS(d: string, s: string): number {
   let base: number = 0;
 
   base = s.length % 2 == 0 ? countVowels(d) * 1.5 : countConsonants(d);
   return getCommonFactor(s.length, d.length) > 1 ? base * 1.5 : base;
 }
 
-function getScores(
+export function getScores(
   drivers: string[],
   streetNames: string[]
 ): [number[][], number] {
